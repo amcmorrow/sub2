a068b934931bf548185f42eeeb851470d42b4e00
amcmorrow <andrew.michael.mcmorrow@gmail.com>
2024-12-19 07:15:40 -0600

    remove tabs

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -33,9 +33,6 @@ new class extends Component
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('tabs')" :active="request()->routeIs('tabs')" wire:navigate>
-                        {{ __('tabs') }}
-                    </x-nav-link>
                 </div>
             </div>
 
