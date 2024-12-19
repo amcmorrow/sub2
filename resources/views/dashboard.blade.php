a7e8837600ed76d8c4c84cf9684c8b686262649c
amcmorrow <andrew.michael.mcmorrow@gmail.com>
2024-12-19 13:06:28 -0600

    dashboard to tabs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,5 @@
+@include('partials.head')
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -9,8 +11,18 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                    Have fun with your life.
+
+<!-- Google Search Bar -->
+<div class="search-bar uk-flex-center" style="display:flex; margin-bottom:15px;">
+    <form action="https://www.google.com/search" method="GET" target="_blank">
+        <input type="text" name="q" placeholder="Search Google..." required>
+        <button class="uk-button uk-button-primary" type="submit">Search</button>
+    </form>
+</div>
+
+@include('partials.randimage')
+
+
                 </div>
             </div>
         </div>
